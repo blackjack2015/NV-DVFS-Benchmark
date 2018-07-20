e8da8abec0698940132eb84b1255cddde3ae26f9
shyhuai <shaohuaishi@gmail.com>
2018-07-20 12:10:56 +0800

    bugs fixed.

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-python dvfs_benchmark.py --benchmark-setting p100 --kernel-setting synthetic --app-root applications/linux
-# python dvfs_benchmark.py --benchmark-setting p100 --kernel-setting real --app-root applications/linux
+# python dvfs_benchmark.py --benchmark-setting p100 --kernel-setting synthetic --app-root applications/linux
+python dvfs_benchmark.py --benchmark-setting p100 --kernel-setting real --app-root applications/linux
