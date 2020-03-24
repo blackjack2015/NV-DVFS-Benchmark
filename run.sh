d5fb800e6794a916f741668b9e75fc05a0c78ed1
blackjack2015 <qiangwang@comp.hkbu.edu.hk>
2020-03-24 19:20:52 +0800

    update v100 data.

--- run.sh
+++ run.sh
@@ -1,24 +1,18 @@
+gpu=v100-dvfs
+kernels=real
+os=linux
+perf_algo=qiang2018
+power_algo=xgboost
+
 # run benchmarks
-#python dvfs_benchmark.py --benchmark-setting p100 --kernel-setting synthetic --app-root applications/linux
-#python dvfs_benchmark.py --benchmark-setting p100 --kernel-setting real --app-root applications/linux
-#python dvfs_benchmark.py --benchmark-setting p100-dvfs --kernel-setting real --app-root applications/linux
-#python dvfs_benchmark.py --benchmark-setting titanx --kernel-setting spgemm --app-root applications/linux
+python dvfs_benchmark.py --benchmark-setting ${gpu} --kernel-setting ${kernels} --app-root applications/${os}
+
+# extract performance data
+python gpuPerfExtractor.py --benchmark-setting ${gpu} --kernel-setting ${kernels}
 
 # use analytical model to fit performance data
-python analytical.py --data-root raw --benchmark-setting gtx980-low-dvfs --kernel-setting real-small-workload --method qiang2018
-python analytical.py --data-root raw --benchmark-setting gtx1080ti-dvfs --kernel-setting real --method qiang2018
-python analytical.py --data-root raw --benchmark-setting p100-dvfs --kernel-setting real --method qiang2018
+python analytical.py --data-root raw --benchmark-setting ${gpu} --kernel-setting ${kernels} --method ${perf_algo}
 
-## use ML model to fit power data
-#python power_dvfs.py --benchmark-setting gtx980-low-dvfs --kernel-setting real-small-workload --method svr-poly
-#python power_dvfs.py --benchmark-setting gtx980-low-dvfs --kernel-setting real-small-workload --method svr-rbf
-#python power_dvfs.py --benchmark-setting gtx980-low-dvfs --kernel-setting real-small-workload --method xgboost
-##python power_dvfs.py --benchmark-setting gtx980-high-dvfs --kernel-setting real-small-workload --method svr-poly
-##python power_dvfs.py --benchmark-setting gtx980-high-dvfs --kernel-setting real-small-workload --method svr-rbf
-##python power_dvfs.py --benchmark-setting gtx980-high-dvfs --kernel-setting real-small-workload --method xgboost
-#python power_dvfs.py --benchmark-setting gtx1080ti-dvfs --kernel-setting real --method svr-poly
-#python power_dvfs.py --benchmark-setting gtx1080ti-dvfs --kernel-setting real --method svr-rbf
-#python power_dvfs.py --benchmark-setting gtx1080ti-dvfs --kernel-setting real --method xgboost
-#python power_dvfs.py --benchmark-setting p100-dvfs --kernel-setting real --method svr-poly
-#python power_dvfs.py --benchmark-setting p100-dvfs --kernel-setting real --method svr-rbf
-#python power_dvfs.py --benchmark-setting p100-dvfs --kernel-setting real --method xgboost
+# use ML model to fit power data
+python power_dvfs.py --benchmark-setting ${gpu} --kernel-setting ${kernels} --method${power_algo}
+
