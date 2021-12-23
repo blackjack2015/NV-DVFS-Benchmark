ea417834099703f0df43e0d029c5d6be638e96e8
blackjack2015 <blackjack201303@gmail.com>
2021-12-23 19:32:12 +0800

    initialize the wsl branch.

--- dvfs_benchmark.py
+++ dvfs_benchmark.py
@@ -3,16 +3,17 @@ import argparse
 import subprocess
 import time
 import re
-import ConfigParser
+import configparser
 import json
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--benchmark-setting', type=str, help='gpu benchmark setting', default='p100')
 parser.add_argument('--kernel-setting', type=str, help='kernels of benchmark', default='synthetic')
+parser.add_argument('--mode', type=str, help='benchmark mode', default='fast', choices=['fast', 'verbose'])
 parser.add_argument('--app-root', type=str, help='folder of applications', default='applications/linux')
 
 opt = parser.parse_args()
-print opt
+print(opt)
 
 BS_SETTING = '%s.cfg' % opt.benchmark_setting
 KS_SETTING = '%s.cfg' % opt.kernel_setting
@@ -26,9 +27,9 @@ except OSError:
     pass
 
 # Reading benchmark settings
-cf_bs = ConfigParser.SafeConfigParser()
+cf_bs = configparser.ConfigParser()
 cf_bs.read("configs/benchmarks/%s" % BS_SETTING)
-cf_ks = ConfigParser.SafeConfigParser()
+cf_ks = configparser.ConfigParser()
 cf_ks.read("configs/kernels/%s" % KS_SETTING)
 
 running_iters = cf_bs.getint("profile_control", "iters")
@@ -49,26 +50,20 @@ else:
 # Read GPU application settings
 benchmark_programs = cf_ks.sections()
 
-print benchmark_programs
-print metrics
-print core_frequencies
-print memory_frequencies
+print(benchmark_programs)
+print(metrics)
+print(core_frequencies)
+print(memory_frequencies)
 
-if 'linux' in sys.platform:
-    pw_sampling_cmd = 'nohup ./nvml_samples -device=%d -si=%d -output=%s/%s 1>null 2>&1 &'
-    app_exec_cmd = './%s/%s %s -device=%d -secs=%d >> %s/%s'
-    dvfs_cmd = 'gpu=%d fcore=%s fmem=%s ./adjustClock.sh' % (nvIns_dev_id, '%s', '%s')
-    kill_pw_cmd = 'killall nvml_samples'
-elif 'win' in sys.platform:
-    pw_sampling_cmd = 'start /B nvml_samples.exe -device=%d -si=%d -output=%s/%s > nul'
-    app_exec_cmd = '%s\\%s %s -device=%d -secs=%d >> %s/%s'
-    if powerState !=0:
-        dvfs_cmd = 'nvidiaInspector.exe -forcepstate:%s,%d -setGpuClock:%s,%d,%s -setMemoryClock:%s,%d,%s' \
-                        % (nvIns_dev_id, powerState, nvIns_dev_id, freqState, '%s', nvIns_dev_id, freqState, '%s')
-    else:
-        dvfs_cmd = 'nvidiaInspector.exe -setBaseClockOffset:%s,%d,%s -setMemoryClockOffset:%s,%d,%s' \
-                        % (nvIns_dev_id, freqState, '%s', nvIns_dev_id, freqState, '%s')
-    kill_pw_cmd = 'tasklist|findstr "nvml_samples.exe" && taskkill /F /IM nvml_samples.exe'
+pw_sampling_cmd = 'nohup ./nvml_samples.exe -device=%d -si=%d -output=%s/%s 1>null 2>&1 &'
+app_exec_cmd = './%s/%s.exe %s -device=%d -secs=%d >> %s/%s'
+kill_pw_cmd = 'killall nvml_samples.exe'
+if powerState !=0:
+    dvfs_cmd = './nvidiaInspector.exe -forcepstate:%s,%d -setGpuClock:%s,%d,%s -setMemoryClock:%s,%d,%s' \
+                    % (nvIns_dev_id, powerState, nvIns_dev_id, freqState, '%s', nvIns_dev_id, freqState, '%s')
+else:
+    dvfs_cmd = './nvidiaInspector.exe -setBaseClockOffset:%s,%d,%s -setMemoryClockOffset:%s,%d,%s' \
+                    % (nvIns_dev_id, freqState, '%s', nvIns_dev_id, freqState, '%s')
 
 for core_f in core_frequencies:
     for mem_f in memory_frequencies:
@@ -76,7 +71,7 @@ for core_f in core_frequencies:
         # set specific frequency
         command = dvfs_cmd % (core_f, mem_f)
         
-        print command
+        print(command)
         os.system(command)
         time.sleep(rest_int)
 
@@ -96,14 +91,14 @@ for core_f in core_frequencies:
                 # start record power data
                 os.system("echo \"arg:%s\" > %s/%s" % (arg, LOG_ROOT, powerlog))
                 command = pw_sampling_cmd % (nvIns_dev_id, pw_sample_int, LOG_ROOT, powerlog)
-                print command
+                print(command)
                 os.system(command)
                 time.sleep(rest_int)
 
                 # execute program to collect power data
                 os.system("echo \"arg:%s\" > %s/%s" % (arg, LOG_ROOT, perflog))
                 command = app_exec_cmd % (APP_ROOT, app, arg, cuda_dev_id, running_time, LOG_ROOT, perflog)
-                print command
+                print(command)
                 os.system(command)
                 time.sleep(rest_int)
 
@@ -111,33 +106,34 @@ for core_f in core_frequencies:
                 os.system(kill_pw_cmd)
 
                 # execute program to collect time data
-                command = 'nvprof --profile-child-processes %s/%s %s -device=%d -secs=5 >> %s/%s 2>&1' % (APP_ROOT, app, arg, cuda_dev_id, LOG_ROOT, perflog)
-                print command
+                command = 'nvprof.exe --profile-child-processes %s/%s %s -device=%d -secs=5 >> %s/%s 2>&1' % (APP_ROOT, app, arg, cuda_dev_id, LOG_ROOT, perflog)
+                print(command)
                 os.system(command)
                 time.sleep(rest_int)
 
-                # collect grid and block settings
-                command = 'nvprof --print-gpu-trace --profile-child-processes %s/%s %s -device=%d -iters=10 > %s/%s 2>&1' % (APP_ROOT, app, arg, cuda_dev_id, LOG_ROOT, metricslog)
-                print command
-                os.system(command)
-                time.sleep(rest_int)
-
-                # execute program to collect metrics data
-                metCount = 0
-
-                # to be fixed, the stride should be a multiplier of the metric number
-                stride = 2
-                while metCount < len(metrics):
-
-                    if metCount + stride > len(metrics):
-                        metStr = ','.join(metrics[metCount:])
-                    else:
-                        metStr = ','.join(metrics[metCount:metCount + stride])
-                    command = 'nvprof --devices %s --metrics %s %s/%s %s -device=%d -iters=10 >> %s/%s 2>&1' % (cuda_dev_id, metStr, APP_ROOT, app, arg, cuda_dev_id, LOG_ROOT, metricslog)
-                    print command
+                if opt.mode == 'verbose':
+                    # collect grid and block settings
+                    command = 'nvprof.exe --print-gpu-trace --profile-child-processes %s/%s %s -device=%d -iters=10 > %s/%s 2>&1' % (APP_ROOT, app, arg, cuda_dev_id, LOG_ROOT, metricslog)
+                    print(command)
                     os.system(command)
-                    time.sleep(1)
-                    metCount += stride
+                    time.sleep(rest_int)
+
+                    # execute program to collect metrics data
+                    metCount = 0
+
+                    # to be fixed, the stride should be a multiplier of the metric number
+                    stride = 2
+                    while metCount < len(metrics):
+
+                        if metCount + stride > len(metrics):
+                            metStr = ','.join(metrics[metCount:])
+                        else:
+                            metStr = ','.join(metrics[metCount:metCount + stride])
+                        command = 'nvprof.exe --devices %s --metrics %s %s/%s %s -device=%d -iters=10 >> %s/%s 2>&1' % (cuda_dev_id, metStr, APP_ROOT, app, arg, cuda_dev_id, LOG_ROOT, metricslog)
+                        print(command)
+                        os.system(command)
+                        time.sleep(1)
+                        metCount += stride
 
 
 time.sleep(rest_int)
