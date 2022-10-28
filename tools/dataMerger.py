13f60c2161bdc2b87d9e99c95ce4af88d87fbc29
blackjack2015 <qiangwang@comp.hkbu.edu.hk>
2022-10-28 20:42:43 +0800

    merge profiling and ptx data.

---
+++ tools/dataMerger.py
@@ -0,0 +1,36 @@
+import json
+import pandas as pd
+import argparse
+import sys
+import configparser
+
+
+def main():
+    """Main function."""
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--ptx-csv', type=str, default='ptx.csv')
+    parser.add_argument('--kernel-setting', type=str)
+    parser.add_argument('--profile-csv', type=str)
+    parser.add_argument('--output', type=str, default='test.csv')
+
+    args = parser.parse_args()
+    print(args)
+
+    # Read GPU application settings
+    cfg_name = 'configs/kernels/%s.cfg' % args.kernel_setting
+    cf_ks = configparser.ConfigParser()
+    cf_ks.read(cfg_name)
+    benchmark_programs = cf_ks.sections()
+
+    app_kernel = {}
+    for app in benchmark_programs:
+        app_kernel[app] = json.loads(cf_ks.get(app, 'kernels'))[0]
+
+    ptx_stats = pd.read_csv(args.ptx_csv, header=0)
+    for (app, kernel) in app_kernel.items():
+        recs = ptx_stats[(ptx_stats.benchmark == app) & (kernel in ptx_stats.kernel)]
+        print(len(recs))
+
+if __name__ == '__main__':
+    main()
