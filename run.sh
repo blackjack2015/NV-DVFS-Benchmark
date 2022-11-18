0e70f75f6ca8fe0ff877438aa008993a1a11f452
popboy126 <hucheng.lew@qq.com>
2022-11-18 08:57:52 +0800

    fix some bugs and run microbenchmarks on rtx8000.

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
-python tools/gpuParse.py --kernel-setting real
-python tools/gpuParse.py --kernel-setting microbenchmark
+GPU=rtx8000-dvfs
 
-python tools/dataMerge.py --profile-csv data/v100-dvfs-real-profile.csv --output data/v100-dvfs-real.csv
-python tools/dataMerge.py --profile-csv data/v100-dvfs-microbenchmark-profile.csv --output data/v100-dvfs-microbenchmark.csv
+python tools/gpuParse.py --benchmark-setting ${GPU} --kernel-setting real
+python tools/gpuParse.py --benchmark-setting ${GPU} --kernel-setting microbenchmark
+
+python tools/dataMerger.py --profile-csv data/${GPU}-real-profile.csv --output data/${GPU}-real.csv
+python tools/dataMerger.py --profile-csv data/${GPU}-microbenchmark-profile.csv --output data/${GPU}-microbenchmark.csv
 
